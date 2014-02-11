885e0dcee3e2a13168312b5138c93840efb0e9f2
Huynh Quang Thao <huynhquangthao@gmail.com>
2014-02-12 03:17:05 +0700

    add comment for spritebatcher. need rework more

--- code/FlappyBirdBattle/flappybirdbattle/src/main/java/com/qthstudios/game/flappybirdbattle/framework/gl/SpriteBatcher.java
+++ code/FlappyBirdBattle/flappybirdbattle/src/main/java/com/qthstudios/game/flappybirdbattle/framework/gl/SpriteBatcher.java
@@ -6,14 +6,21 @@ import android.util.FloatMath;
 import com.qthstudios.game.flappybirdbattle.framework.impl.GLGraphics;
 import com.qthstudios.game.flappybirdbattle.framework.math.Vector2;
 
+/**
+ *
+ */
 public class SpriteBatcher {
+    // store the vertices of the sprites of the current batch in
     final float[] verticesBuffer;
+    // where in float buffer array we start to write next vertices
     int bufferIndex;
     final Vertices vertices;
+    // store number of sprites in current batch
     int numSprites;    
     
     public SpriteBatcher(GLGraphics glGraphics, int maxSprites) {
-        this.verticesBuffer = new float[maxSprites*4*4];        
+        // four vertices per sprite. each vertex take 4 floats (2 for x-y coordinate and 2 for texture coordinate)
+        this.verticesBuffer = new float[maxSprites*4*4];
         this.vertices = new Vertices(glGraphics, maxSprites*4, maxSprites*6, false, true);
         this.bufferIndex = 0;
         this.numSprites = 0;
@@ -30,21 +37,41 @@ public class SpriteBatcher {
                 indices[i + 5] = (short)(j + 0);
         }
         vertices.setIndices(indices, 0, indices.length);                
-    }       
-    
+    }
+
+    /**
+     * Usage pattern of SpriteBatcher:
+     * SpriteBatcher.beginBatch(texture);
+     // call batcher.drawSprite() as often as needed, referencing regions in the texture
+     SpriteBatcher.endBatch();
+     */
+
+    /**
+     * It binds the texture and resets the numSprites and bufferIndex members
+     * so the first sprite’s vertices will get inserted at the front of the  verticesBuffer float array.
+     */
     public void beginBatch(Texture texture) {
         texture.bind();
         numSprites = 0;
         bufferIndex = 0;
     }
-    
+
+    // signals to the sprite batcher that we are done rendering the batch of sprites.
+    // Now it should now upload the vertices to the GPU for actual rendering.
+    // We are going to use indexed rendering with a Vertices instance, so we’ll also need to specify indices,
+    // in addition to the vertices in the float array buffer.
+    // However, since we are always rendering rectangles, we can generate the indices
+    // beforehand once in the constructor of the SpriteBatcher.
+    // For this we need to know how many sprites the batcher should be able to draw maximally per batch.
+    // By putting a hard limited on the number of sprites that can be rendered per batch, we don’t need to grow
+    // any arrays of other buffers; we can just allocate these arrays and buffers once in the constructor.
     public void endBatch() {
         vertices.setVertices(verticesBuffer, 0, bufferIndex);
         vertices.bind();
         vertices.draw(GL10.GL_TRIANGLES, 0, numSprites * 6);
         vertices.unbind();
     }
-    
+
     public void drawSprite(float x, float y, float width, float height, TextureRegion region) {
         float halfWidth = width / 2;
         float halfHeight = height / 2;
