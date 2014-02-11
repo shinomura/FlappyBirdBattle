3e11de61f258ff61a2b4396050f6a06366131bd4
Dinh Quang Trung <trungdq88@gmail.com>
2014-02-10 19:37:31 -0800

    Intergrate graphics. Rewrite Assets

--- code/FlappyBirdBattle/flappybirdbattle/src/main/java/com/qthstudios/game/flappybirdbattle/utils/ResourceUtils.java
+++ code/FlappyBirdBattle/flappybirdbattle/src/main/java/com/qthstudios/game/flappybirdbattle/utils/ResourceUtils.java
@@ -1,6 +1,7 @@
 package com.qthstudios.game.flappybirdbattle.utils;
 
 
+import android.content.Context;
 import android.graphics.drawable.Drawable;
 import com.qthstudios.game.flappybirdbattle.screen.BunnyApplication;
 
@@ -8,6 +9,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /** contain useful methods for loading resource from system */
 public class ResourceUtils {
@@ -19,7 +22,25 @@ public class ResourceUtils {
     public static Drawable getDrawableFromResId(int id) {
         return BunnyApplication.getAppContext().getResources().getDrawable(id);
     }
+    public static String readRawTextFile(Context ctx, int resId)
+    {
+        InputStream inputStream = ctx.getResources().openRawResource(resId);
 
+        InputStreamReader inputreader = new InputStreamReader(inputStream);
+        BufferedReader buffreader = new BufferedReader(inputreader);
+        String line;
+        StringBuilder text = new StringBuilder();
+
+        try {
+            while (( line = buffreader.readLine()) != null) {
+                text.append(line);
+                text.append('\n');
+            }
+        } catch (IOException e) {
+            return null;
+        }
+        return text.toString();
+    }
     public static String readFile(String path) {
         //Get the text file
         File file = new File(path);
